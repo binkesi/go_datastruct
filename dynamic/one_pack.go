7d78d32ea2830626f4e677c8ba8b465b82b466a4
binkesi <sjtusgn@163.com>
2021-09-13 18:19:52 +0800

    Parse data backwards.

--- dynamic/one_pack.go
+++ dynamic/one_pack.go
@@ -9,16 +9,16 @@ func OnePack(Weight []int, col int) int {
 		state[Weight[0]] = 1
 	}
 	for i := 1; i < row; i++ {
-		for j := 0; j <= col; j++ {
+		for j := col; j >= 0; j-- {
 			if state[j] == 1 && j+Weight[i] <= col {
 				state[j+Weight[i]] = 1
 			}
-			if Weight[i] <= col {
-				state[Weight[i]] = 1
-			}
 		}
+		if Weight[i] <= col {
+			state[Weight[i]] = 1
+		}
+		fmt.Printf("Package state result: %v\n", state)
 	}
-	fmt.Printf("Package state result: %v", state)
 	for k := col; k >= 0; k-- {
 		if state[k] == 1 {
 			return k
